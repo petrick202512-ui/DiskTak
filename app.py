66c315d3b0d2603d7a038292c44247bc9dbf4515
petrick202512-ui <petrick202512@gmail.com>
2026-06-04 11:05:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ def handle_stream_end(data):
     }, skip_sid=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
